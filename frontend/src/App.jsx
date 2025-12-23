4cf6d8e4b530c7a0f0220b55b194390b3d4dea1d
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-12-23 10:58:27 +0500

    Created app structure and added react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import {Routes, Route} from 'react-router-dom';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <div>
+      <Routes>
+        <Route path="/protected" element={
+          <ProtectedRoute>
+            <div>Protected Content</div>
+          </ProtectedRoute>
+        } />
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
+
+      </Routes>
+    </div>
   )
 }
 
