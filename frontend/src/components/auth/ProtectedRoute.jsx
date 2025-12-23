8d8aa6f8d190c8f17bc14a69ff2a13e479af33e1
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-12-23 12:21:04 +0500

    added protected login routes

--- frontend/src/components/auth/ProtectedRoute.jsx
+++ frontend/src/components/auth/ProtectedRoute.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useLocation, Navigate } from 'react-router-dom'
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+
+const ProtectedRoute = ( {children}) => {
+  const isAuthencated = false // Replace with actual authentication logic
+  const loading = false // Replace with actual loading state
+  const location = useLocation();
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  if (!isAuthencated) {
+    return <Navigate to="/login" state={{ from: location }} replace />
+  }
+  // eslint-disa ble-next-line no-undef
+  return children;  
 }
 
 export default ProtectedRoute
