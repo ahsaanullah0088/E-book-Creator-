cefcee801cc5f0dfdebb379d0ee6d9d27e427e67
Ahsaan Ullah <ahsaanu70@gmail.com>
2026-01-15 08:28:24 +0500

    update book , delete book controller

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -39,7 +39,7 @@ const createBook = async (req, res) => {
 
 const getBooks = async (req, res) => {
   try {
-    const books = await Book.find({user : req.user._id}).sort({createdAt : -1});
+    const books = await Book.find({userId : req.user._id}).sort({createdAt : -1});
     res.status(200).json({books});
 
   } catch (error) {
@@ -56,7 +56,7 @@ const getBookById = async (req, res) => {
     if(!book){
         return res.status(404).json({message : "Book not found"});
     }
-    if(book.user.toString() !== req.user._id.toString()){
+    if(book.userId.toString() !== req.user._id.toString()){
         return res.status(401).json({message : "Not authorized to view this book"});
     }
     res.status(200).json({book});
@@ -68,6 +68,15 @@ const getBookById = async (req, res) => {
 // update a book
 const updateBook = async (req, res) => {
   try {
+    const book = await Book.findById(req.params.id);
+    if(!book){
+        return res.status(404).json({message : "Book not found"});
+    }
+    if(book.userId.toString() !== req.user._id.toString()){
+        return res.status(401).json({message : "Not authorized to update this book"});
+    }
+    const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body, {new : true});
+    res.status(200).json({message : "Book updated successfully", book : updateBook});
   } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
@@ -76,7 +85,16 @@ const updateBook = async (req, res) => {
 /// delete a book
 
 const deleteBook = async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  if(!book){
+      return res.status(404).json({message : "Book not found"});
+  }
+  if(book.userId.toString() !== req.user._id.toString()){
+      return res.status(401).json({message : "Not authorized to delete this book"});
+  }
   try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.status(200).json({message : "Book deleted successfully"});
   } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
